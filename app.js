0f32ac1136521c6eb938a0e33fd62e87172df681
Patrick Dave Intervalo <72492512+patrickdavein@users.noreply.github.com>
2020-10-30 22:55:01 +0800

    gthb bckp commit

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
-toggleBtn.addEventListener("click", function () {
+
+toggleBtn.addEventListener("click", () => {
   // if (sidebar.classList.contains("show-sidebar")) {
   //   sidebar.classList.remove("show-sidebar");
   // } else {
@@ -11,6 +12,7 @@ toggleBtn.addEventListener("click", function () {
   sidebar.classList.toggle("show-sidebar");
 });
 
-closeBtn.addEventListener("click", function () {
+
+closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("show-sidebar");
 });
